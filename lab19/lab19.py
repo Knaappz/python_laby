2d9f4a97940e047c7afa17d7824d53ab560a1028
Jakub <knapikj.priv@gmail.com>
2023-05-31 12:08:37 +0200

    Lab19 z1, z plikami

---
+++ lab19/lab19.py
@@ -0,0 +1,23 @@
+def sprawdz_kod(kod):
+    if not kod[:2].isdigit():
+        raise ValueError("Ciąg liczb jest niepoprawny")
+    elif kod[2] != '-':
+        raise ValueError("Ciąg liczb jest niepoprawny")
+    elif not kod[3:].isdigit():
+        raise ValueError("Ciąg liczb jest niepoprawny")
+
+with open("kody_pocztowe.txt", "a") as file:
+    while True:
+        kod = input("Podaj kod pocztowy (wpisz 'done' aby zakończyć): ")
+        if kod == "done":
+            print("Do zobaczenia :)")
+            break
+
+        try:
+            sprawdz_kod(kod)
+            print("Podany kod jest poprawny")
+
+            file.write(kod + "\n")
+            print("Kod pocztowy został zapisany do pliku 'kody_pocztowe.txt'")
+        except ValueError as e:
+            print("Błąd:", str(e))
